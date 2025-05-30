bf99a7d89460a3c9eb4a0c1c12a03b86bed8bef0
oluquejs <luquedevjs@icloud.com>
2025-05-30 14:49:30 -0300

    feat(pokemon): create type PokeSprites

---
+++ src/modules/pokemon/types/pokesprites.type.ts
@@ -0,0 +1,12 @@
+export type PokeSprites = {
+  back_default: SpriteUrl;
+  back_female: SpriteUrl;
+  back_shiny: SpriteUrl;
+  back_shiny_female: SpriteUrl;
+  front_default: SpriteUrl;
+  front_female: SpriteUrl;
+  front_shiny: SpriteUrl;
+  front_shiny_female: SpriteUrl;
+}
+
+type SpriteUrl = string|null

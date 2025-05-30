8dfabf5fb9017d65122461b223d813b0da9c1be3
oluquejs <luquedevjs@icloud.com>
2025-05-30 15:40:08 -0300

    feat(pokemon): implement getPokemonCharacteristics route

--- src/modules/pokemon/pokemon.controller.ts
+++ src/modules/pokemon/pokemon.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { PokemonService } from "./pokemon.service";
 import { PokemonDTO } from "./dto/pokemon.dto";
+import { PokemonCharacteristicsDTO } from "./dto/pokemon-characteristics.dto";
 
 @Controller('pokemon')
 export class PokemonController {
@@ -13,5 +14,12 @@ export class PokemonController {
     @Param('pokeI') pokeI: string
   ): Promise<PokemonDTO> {
     return await this.pokemonService.getPokemonInfoByName(pokeI);
+  }
+
+  @Get('/characteristic/:pokeI')
+  async getPokemonCharacteristics(
+    @Param('pokeI') pokeI: string
+  ): Promise<PokemonCharacteristicsDTO> {
+    return await this.pokemonService.getPokemonCharacteristics(pokeI)
   }
 }
